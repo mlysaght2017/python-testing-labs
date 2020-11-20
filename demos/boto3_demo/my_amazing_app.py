5acfa420ba2d6246b5b7b02b4ae52b1fbad0ebe2
EC2 Default User <ec2-user@ip-172-31-29-106.us-east-2.compute.internal>
2020-11-20 15:53:09 +0000

    Formatting

--- demos/boto3_demo/my_amazing_app.py
+++ demos/boto3_demo/my_amazing_app.py
@@ -2,19 +2,22 @@ import boto3
 from botocore.exceptions import ClientError
 import os
 
+
 def get_variable(var_name):
     response = boto3.client("ssm").get_parameter(Name=var_name)
-    return response['Parameter']['Value']
-    
+    return response["Parameter"]["Value"]
+
 
 def store_data(dir, bucket, key):
-    
-    tmp = '/tmp'
-    zip_file = 'my_file'
+
+    tmp = "/tmp"
+    zip_file = "my_file"
     try:
-        boto3.resource('s3').Object(bucket, key).upload_file(os.path.join(tmp, zip_file))
+        boto3.resource("s3").Object(bucket, key).upload_file(
+            os.path.join(tmp, zip_file)
+        )
         message = "Pass"
     except ClientError as ex:
         message = "Fail"
-    
-    return message    
+
+    return message
--- demos/boto3_demo/my_amazing_app_test.py
+++ demos/boto3_demo/my_amazing_app_test.py
@@ -4,42 +4,36 @@ import botocore
 import my_amazing_app
 
 
-
 class TestMyAmazingApp(unittest.TestCase):
-    
-    @patch('my_amazing_app.boto3.client')
+    @patch("my_amazing_app.boto3.client")
     def test_get_variable(self, mock_boto3_client):
-        
-        response = {
-            'Parameter': {
-                'Name': 'MyTestParameterName',
-                'Value': 'myValue'
-            }
-        }
-        
-        #Reason for the next line is that by default mock creates a new mock object for all return values not explicitly set.
-        #Without explicitly setting the return value, the object that get_parameter() is being called on would be different to the 
-        #one we are attempting to set a return value on.
-        mock_boto3_client.return_value = mock_boto3_client 
+
+        response = {"Parameter": {"Name": "MyTestParameterName", "Value": "myValue"}}
+
+        # Reason for the next line is that by default mock creates a new mock object for all return values not explicitly set.
+        # Without explicitly setting the return value, the object that get_parameter() is being called on would be different to the
+        # one we are attempting to set a return value on.
+        mock_boto3_client.return_value = mock_boto3_client
         mock_boto3_client.get_parameter.return_value = response
-    
+
         result_value = my_amazing_app.get_variable("MyTestParameterName")
-        
-        mock_boto3_client.assert_called_with('ssm')
+
+        mock_boto3_client.assert_called_with("ssm")
         mock_boto3_client().get_parameter.assert_called_with(Name="MyTestParameterName")
-        
+
         self.assertEqual("myValue", result_value)
-    
-    @patch('my_amazing_app.boto3.resource')    
+
+    @patch("my_amazing_app.boto3.resource")
     def test_store_data_unauthorized(self, boto3_resource):
-        boto3_resource.return_value.Object.return_value.upload_file.side_effect = botocore.exceptions.ClientError(
-            {"Error": {"Code": "403", "Message": "Unauthorized"}}, "PutObject"    
+        boto3_resource.return_value.Object.return_value.upload_file.side_effect = (
+            botocore.exceptions.ClientError(
+                {"Error": {"Code": "403", "Message": "Unauthorized"}}, "PutObject"
+            )
         )
         return_value = my_amazing_app.store_data("test", "bucket", "key")
         boto3_resource.assert_called_with("s3")
         self.assertEqual("Fail", return_value)
-        
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
-        
