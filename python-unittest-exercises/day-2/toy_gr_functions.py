6a294b0fa756a34e82e1345c58358192d2f49747
EC2 Default User <ec2-user@ip-172-31-29-106.us-east-2.compute.internal>
2020-11-20 13:04:52 +0000

    Adding more exercise content

--- python-unittest-exercises/day-2/toy_gr_functions.py
+++ python-unittest-exercises/day-2/toy_gr_functions.py
@@ -14,3 +14,35 @@ def remove_iam_user_ssh_public_key_upload(user_name, ssh_pub_key_id):
         message = "fail"
         
     return message    
+
+def enable_s3_public_access_block(bucket_name):
+    
+    try:
+        response = boto3.client("s3").put_public_access_block(
+            Bucket=bucket_name,
+            PublicAccessBlockConfiguration={
+                "BlockPublicAcls": True,
+                "IgnorePublicAcls": True,
+                "BlockPublicPolicy": True,
+                "RestrictPublicBuckets": True,
+            }
+        )
+        message = "success"
+    except ClientError as ex:
+        message = "fail"
+    
+
+def check_public_access_block(bucket_name):
+    
+    try:
+        response = boto3.client("s3").get_public_access_block(Bucket=bucket_name)
+        blocked_pub_acl = response["PublicAccessBlockConfiguration"]["BlockPublicAcls"]
+        ignore_pub_acls = response["PublicAccessBlockConfiguration"]["IgnorePublicAcls"]
+        blocked_pub_pol = response["PublicAccessBlockConfiguration"]["BlockPublicPolicy"]
+        restrict_public = response["PublicAccessBlockConfiguration"]["RestrictPublicBuckets"]
+        
+        return bool(
+            blocked_pub_acl and ignore_pub_acls and blocked_pub_pol and restrict_public
+        )
+    except ClientError as ex:
+        return False
